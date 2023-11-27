2b0f662b9aaaa24c0614df8048b002587a9b6bbc
jason <saga1654@kakao.com>
2023-11-27 15:06:02 +0900

    유효성 검증 jar 실행

--- chapter04/src/main/kotlin/io/spring/boot/chapter04/job/HelloWorldJob.kt
+++ chapter04/src/main/kotlin/io/spring/boot/chapter04/job/HelloWorldJob.kt
@@ -1,8 +1,11 @@
 package io.spring.boot.chapter04.job
 
+import io.spring.boot.chapter04.batch.ParameterValidator
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.StepScope
+import org.springframework.batch.core.job.CompositeJobParametersValidator
+import org.springframework.batch.core.job.DefaultJobParametersValidator
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
@@ -13,7 +16,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
 
-//@Configuration
+ @Configuration
 class HelloWorldJob(
     private val jobRepository: JobRepository,
     private val transactionManager: PlatformTransactionManager,
@@ -22,19 +25,37 @@ class HelloWorldJob(
     @Bean
     fun job(): Job {
         System.out.println("HelloWorldJob.job()")
-        return JobBuilder("basicJob", jobRepository).start(step1()).build()
+        return JobBuilder("basicJob", jobRepository).start(step1()).validator(validator()).build()
     }
 
     @Bean
     fun step1(): Step {
         System.out.println("HelloWorldJob.step1()")
-        return StepBuilder("step1", jobRepository).tasklet(helloWorldTasklet(name = null), transactionManager).allowStartIfComplete(true).build()
+        return StepBuilder("step1", jobRepository).tasklet(helloWorldTasklet(null, null), transactionManager).allowStartIfComplete(true).build()
+    }
+
+    @Bean
+    fun validator(): CompositeJobParametersValidator {
+        val validator = CompositeJobParametersValidator()
+        val defaultJobParametersValidator = DefaultJobParametersValidator(arrayOf("fileName"), arrayOf("name"))
+
+        defaultJobParametersValidator.afterPropertiesSet()
+
+        validator.setValidators(
+            listOf(
+                ParameterValidator(),
+                defaultJobParametersValidator,
+            ),
+        )
+
+        return validator
     }
 
     @StepScope
     @Bean
     fun helloWorldTasklet(
         @Value("#{jobParameters['name']}") name: String? = null,
+        @Value("#{jobParameters['fileName']}") fileName: String? = null,
     ): Tasklet {
         System.out.println("HelloWorldJob.helloWorldTasklet()")
 
@@ -43,6 +64,12 @@ class HelloWorldJob(
                 println("Hello, World!")
             } else {
                 println("Hello, $name")
+            }
+
+            if (fileName == null) {
+                println("fileName = null")
+            } else {
+                println("fileName = $fileName")
             }
             RepeatStatus.FINISHED
         }

02789856a073f8f2eb292814e8746d843c59161f
jason <saga1654@kakao.com>
2023-11-30 11:35:27 +0900

    도메인

---
+++ chapter06/src/main/kotlin/io/spring/boot/chapter06/domain/AccountSummary.kt
@@ -0,0 +1,7 @@
+package io.spring.boot.chapter06.domain
+
+class AccountSummary {
+    var id: Int = 0
+    var accountNumber: String = ""
+    var currentBalance: Double = 0.0
+}
---
+++ chapter06/src/main/kotlin/io/spring/boot/chapter06/domain/Transaction.kt
@@ -0,0 +1,7 @@
+package io.spring.boot.chapter06.domain
+
+class Transaction {
+    var accountNumber: String = ""
+    var timestamp: Long = 0
+    var amount: Double = 0.0
+}
